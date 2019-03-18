c0e40dd9705ea802ef72746d08a1973eb41ad863
mizunoajigawakaru <46399458+mizunoajigawakaru@users.noreply.github.com>
2019-03-18 21:04:44 +0900

    値表示部分をBezierHeaderコンポーネントに分離

---
+++ src/components/BezierHeader.vue
@@ -0,0 +1,76 @@
+<template>
+<div class="bezier-header">
+  <svg
+    v-if="selectedPresetType"
+    class="bezier-preset-modify bezier-preset-minus"
+    width="20"
+    height="20"
+    @click="$emit('change-preset', -1)"
+  >
+    <path d="M 12 6 L 8 10 L 12 14" />
+  </svg>
+  <svg
+    v-if="selectedPresetType"
+    class="bezier-preset-modify bezier-preset-plus"
+    width="20"
+    height="20"
+    @click="$emit('change-preset', 1)"
+  >
+    <path d="M 8 6 L 12 10 L 8 14" />
+  </svg>
+  <span class="source-code bezier-display-value">{{ displayValue }}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BezierHeader',
+  props: [
+    'selectedPresetType',
+    'displayValue',
+  ],
+}
+</script>
+
+<style lang="scss" scoped>
+.bezier-header {
+  margin-top: 16px;
+
+  .bezier-display-value {
+    display: block;
+    width: 100%;
+    height: 20px;
+    line-height: 20px;
+    text-align: center;
+    white-space: nowrap;
+
+    .dragging & {
+      -webkit-user-select: none;
+    }
+  }
+
+  .bezier-preset-modify {
+    background-color: #f5f5f5;
+    border-radius: 35px;
+    display: inline-block;
+    transition: transform 100ms cubic-bezier(0.4, 0, 0.2, 1);
+    cursor: pointer;
+    position: absolute;
+
+    &.bezier-preset-plus {
+      right: 16px;
+    }
+
+    &:hover {
+      background-color: #999;
+    }
+  }
+}
+
+.source-code {
+  font-size: 11px;
+  font-family: Menlo, monospace;
+  font-weight: 400;
+  color: #303942;
+}
+</style>
--- src/components/EasingEditor.vue
+++ src/components/EasingEditor.vue
@@ -18,27 +18,11 @@
         @drag-start="dragstart($event)"
       />
     </div>
-    <div class="bezier-header">
-      <svg
-        v-if="selectedPresetType"
-        class="bezier-preset-modify bezier-preset-minus"
-        width="20"
-        height="20"
-        @click="changePreset(-1)"
-      >
-        <path d="M 12 6 L 8 10 L 12 14" />
-      </svg>
-      <svg
-        v-if="selectedPresetType"
-        class="bezier-preset-modify bezier-preset-plus"
-        width="20"
-        height="20"
-        @click="changePreset(1)"
-      >
-        <path d="M 8 6 L 12 10 L 8 14" />
-      </svg>
-      <span class="source-code bezier-display-value">{{ displayValue }}</span>
-    </div>
+    <bezier-header
+      :selected-preset-type="selectedPresetType"
+      :display-value="displayValue"
+      @change-preset="changePreset"
+    />
   </div>
 </template>
 
@@ -47,12 +31,14 @@ import { isEqual } from 'lodash';
 import * as presets from '../constants/presets';
 import BezierPresets from './BezierPresets.vue';
 import BezierPreview from './BezierPreview.vue';
+import BezierHeader from './BezierHeader.vue';
 
 export default {
   name: 'EasingEditor',
   components: {
     BezierPresets,
     BezierPreview,
+    BezierHeader,
   },
   data() {
     return {
@@ -266,6 +252,7 @@ export default {
     },
 
     changePreset(count) {
+      console.log('changePreset', count);
       const currentIndex = this.selectedPresetIndex[this.selectedPresetType];
       const selectedPresetList = presets.PRESET_LISTS[this.selectedPresetType];
       const nextIndex = currentIndex + count === selectedPresetList.length
@@ -300,46 +287,5 @@ export default {
 .bezier-container {
   display: flex;
   margin-top: 38px;
-}
-
-.bezier-header {
-  margin-top: 16px;
-
-  .bezier-display-value {
-    display: block;
-    width: 100%;
-    height: 20px;
-    line-height: 20px;
-    text-align: center;
-    white-space: nowrap;
-
-    .dragging & {
-      -webkit-user-select: none;
-    }
-  }
-
-  .bezier-preset-modify {
-    background-color: #f5f5f5;
-    border-radius: 35px;
-    display: inline-block;
-    transition: transform 100ms cubic-bezier(0.4, 0, 0.2, 1);
-    cursor: pointer;
-    position: absolute;
-
-    &.bezier-preset-plus {
-      right: 16px;
-    }
-
-    &:hover {
-      background-color: #999;
-    }
-  }
-}
-
-.source-code {
-  font-size: 11px;
-  font-family: Menlo, monospace;
-  font-weight: 400;
-  color: #303942;
 }
 </style>
